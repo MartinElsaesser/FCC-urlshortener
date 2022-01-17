0644af21bf4c242bbe90ef47437738cb1ff4ac89
MartinElsaesser <marels@aon.at>
2022-01-17 19:01:49 +0100

    finished

--- server.js
+++ server.js
@@ -2,23 +2,76 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+const mongoose = require("mongoose");
+const Url = require("./models/Url");
+const methodOverride = require("method-override");
 
-// Basic Configuration
+mongoose.connect(`mongodb+srv://${process.env.db_USER}:${process.env.db_PASS}@${process.env.db_HOST}/url-shortener?retryWrites=true&w=majority`);
+mongoose.connection.on('error', err => {
+	console.log(err);
+});
+
+function lookup(url) {
+	return new Promise(function (resolve, reject) {
+		dns.lookup(url, (err, address, family) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve({ address, family });
+			}
+		});
+	})
+}
+
 const port = process.env.PORT || 3000;
-
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', (req, res) => {
+	res.render("index");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/shorturls', async (req, res) => {
+	let urls = await Url.find({});
+	res.render("list", { urls });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+
+app.get("/api/shorturl", async (req, res) => {
+	let urls = await Url.find({});
+	res.json(urls);
+});
+
+app.post("/api/shorturl", async (req, res) => {
+	let regex = /^(?:https?:\/\/)?([^\/]*)/;
+	let path = req.body.url;
+	let checkUrl = path.match(regex)[1];
+	try {
+		await lookup(checkUrl);
+		const url = await Url.create({ path });
+		res.json({ original_url: url.path, short_url: url.short })
+	} catch (error) {
+		res.json({ error: "Invalid URL" })
+	}
+});
+
+app.get("/api/shorturl/:shorturl", async (req, res) => {
+	let short = req.params.shorturl;
+	let url = await Url.findOne({ short });
+	res.redirect(url.path)
+});
+
+app.delete("/api/shorturl/:id", async (req, res) => {
+	let id = req.params.id;
+	let url = await Url.findByIdAndDelete(id);
+	res.redirect("/api/shorturl");
+});
+
+app.listen(port, _ => {
+	console.log(`Listening on port ${port}`);
 });
